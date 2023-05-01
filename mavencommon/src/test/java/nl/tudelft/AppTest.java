0fdfa27f5c391515b7b6dcfdf7af0cdaafff4d6f
ntomassen <n.h.c.tomassen@student.tudelft.nl>
2023-05-01 14:51:04 +0200

    Add common module and change pom java version

---
+++ mavencommon/src/test/java/nl/tudelft/AppTest.java
@@ -0,0 +1,20 @@
+package nl.tudelft;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
