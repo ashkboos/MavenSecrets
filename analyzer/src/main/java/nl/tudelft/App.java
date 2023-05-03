b440a59f974d614d6f950e748d1a3e0e3db6de85
Tudor-Gabriel Velican <t.velican@student.tudelft.nl>
2023-05-03 12:14:04 +0200

    Created logic to pull packageIds from toy dataset database

--- analyzer/src/main/java/nl/tudelft/App.java
+++ analyzer/src/main/java/nl/tudelft/App.java
@@ -5,21 +5,14 @@ import nl.tudelft.mavensecrets.resolver.Resolver;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class App {
     public static void main(String[] args) throws IOException, SQLException {
-        if (args.length != 1)
-            throw new RuntimeException("expected a single package id");
-
-        var id = PackageId.tryParse(args[0]);
-        if (id.isEmpty())
-            throw new RuntimeException("invalid package id");
-
-        var packages = new PackageId[]{
-                id.get()
-        };
+        var db = openDatabase();
+        var packages = db.getPackageIds();
 
         Logger logger = Logger.getGlobal();
         File local = new File(System.getProperty("user.home") + "/.m2/repository");
@@ -28,7 +21,6 @@ public class App {
 
         var builder = extractors(new RunnerBuilder());
         var maven = new Maven(resolver);
-        var db = openDatabase();
         try (var runner = builder.build(db)) {
             runner.run(maven, packages);
         }
@@ -41,4 +33,5 @@ public class App {
     private static Database openDatabase() throws SQLException {
         return Database.connect("jdbc:postgresql://localhost:5432/postgres", "postgres", "SuperSekretPassword");
     }
+
 }
