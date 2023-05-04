a16457e326ea8262fc272871c2c98e039d8cbe84
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-05-04 14:26:36 +0200

    Added vc extractor to runner

--- analyzer/src/main/java/nl/tudelft/App.java
+++ analyzer/src/main/java/nl/tudelft/App.java
@@ -40,7 +40,8 @@ public class App {
                 .addExtractor("compiler", new CompilerConfigExtractor())
                 .addExtractor("modules", new JavaModuleExtractor())
                 .addExtractor("version", new JavaVersionExtractor())
-                .addExtractor("parent", new ParentExtractor());
+                .addExtractor("parent", new ParentExtractor())
+                .addExtractor("repo_urls", new ExtractorVC());
     }
 
     private static Database openDatabase() throws SQLException {
