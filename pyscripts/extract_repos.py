e2a46bf98f305c0271a0aae2c07f03b74713e4d6
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-05-09 17:24:24 +0200

    refactored extract_repos.py

--- pyscripts/extract_repos.py
+++ pyscripts/extract_repos.py
@@ -1,30 +1,48 @@
 from giturlparse import parse
 from database import *
 
-db = Database('localhost', '5432', 'postgres', 'SuperSekretPassword')
-conn = db.connect()
-print("Connected")
-# cur = conn.cursor()
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-cur.execute(
-    'SELECT id, scm_url FROM packages WHERE scm_url IS NOT NULL ORDER BY scm_url')
+# Scenarios
+# 1. ONLY scm_url 
+# 2. ONLY homepage_url
+# 3. scm_url & homepage_url are the same
+# 4. BOTH null
 
-hosts = []
-unparseable = []
+def main():
+    db = Database('localhost', '5432', 'postgres', 'SuperSekretPassword')
+    conn = db.connect()
+    print("Connected")
 
-for record in cur:
-    url = record["scm_url"]
+    parse_url("scm_url", conn)
+    parse_url('homepage_url', conn)
 
-    try:
-        parsed_url = parse(url)
-        host = parsed_url.host
-        hosts.append(host)
-    except:
-        unparseable.append(url)
+    # cur = conn.cursor()
+    # cur.execute('SELECT COUNT(*) FROM packages WHERE scm_url IS NULL')
+    # print('scm_url is NULL =', cur.fetchall()[0][0])
 
-print(sorted([(host, hosts.count(host)) for host in set(hosts)]))
-print('Parsed:', len(hosts))
-print('NOT Parsed:', len(unparseable))
 
-cur.execute('SELECT COUNT(*) FROM packages WHERE scm_url IS NULL')
-print(cur.fetchall())
+def parse_url(field: str, conn):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(
+        'SELECT id, {0} FROM packages WHERE {0} IS NOT NULL ORDER BY {0}'.format(field))
+
+    hosts = []
+    unparseable = []
+
+    for record in cur:
+        url = record[field]
+
+        try:
+            parsed_url = parse(url)
+            host = parsed_url.host
+            hosts.append(host)
+        except:
+            unparseable.append(url)
+
+    print('*' * 50)
+    print(field)
+    print(sorted([(host, hosts.count(host)) for host in set(hosts)]))
+    print('Parsed =', len(hosts))
+    print('NOT Parsed =', len(unparseable))
+
+
+main()
