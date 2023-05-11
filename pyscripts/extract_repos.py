96791ec575e44e2e4794a1a2ba0ae9354c2cb306
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-05-11 09:47:01 +0200

    worked on inserting hostnames into db (currently broken)

--- pyscripts/extract_repos.py
+++ pyscripts/extract_repos.py
@@ -1,4 +1,7 @@
 from urllib.parse import urlparse
+
+from psycopg2.extensions import cursor
+from psycopg2.extras import DictCursor, execute_batch
 from database import *
 import re
 
@@ -12,24 +15,30 @@ import re
 def main():
     db = Database('localhost', '5432', 'postgres', 'SuperSekretPassword')
     conn = db.connect()
+    cur = conn.cursor(cursor_factory=DictCursor)
     print("Connected")
 
-    parse_url("scm_url", conn)
-    parse_url('homepage_url', conn)
+    create_table(cur)
+    process_url("scm_url", cur)
+    process_url('homepage_url', cur)
 
-    cur = conn.cursor()
     print('*' * 50)
     cur.execute('SELECT COUNT(*) FROM packages WHERE scm_url IS NULL')
     print('scm_url is NULL =', cur.fetchall()[0][0])
     cur.execute('SELECT COUNT(*) FROM packages WHERE homepage_url IS NULL')
     print('homepage_url is NULL =', cur.fetchall()[0][0])
 
+    # cleanup
+    cur.close()
+    conn.close()
 
-def parse_url(field: str, conn):
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+# every 100 records, insert the hostnames into new database
+def process_url(field: str, cur: DictCursor):
     cur.execute(
         'SELECT id, {0} FROM packages WHERE {0} IS NOT NULL ORDER BY {0}'.format(field))
-
+    
+    urls = []
     hosts = []
     unparseable = []
 
@@ -42,19 +51,28 @@ def parse_url(field: str, conn):
             host = parse_homepage_url(url)
 
         if host:
+            urls.append(url)
             hosts.append(host)
         else:
             unparseable.append(host)
 
+        if (len(hosts) == 100):
+            insert_hosts(urls, hosts, cur)
+            urls.clear()
+            hosts.clear()
+
+    # TODO final request
+
     print('*' * 50)
     print(field)
-    print([(host, hosts.count(host)) for host in set(hosts)])
+    # print([(host, hosts.count(host)) for host in set(hosts)])
+    # TODO make query to fetch all counts
     print('Parsed =', len(hosts))
     print('NOT Parsed =', len(unparseable))
     return hosts, unparseable
 
 
-def parse_git_url(url):
+def parse_git_url(url) -> str:
     n_url = re.sub(r"^(scm|svn):git:", "", url)
     n_url = re.sub(r"^(scm|svn):", "", n_url)
     n_url = re.sub(r"^git@", "ssh://git@", n_url)
@@ -67,11 +85,25 @@ def parse_git_url(url):
     return parsed_url.hostname
 
 
-def parse_homepage_url(url):
+def parse_homepage_url(url) -> str:
     parsed_url = urlparse(url)
     if parsed_url.hostname == None:
         print(url)
     return parsed_url.hostname
 
+
+def insert_hosts(urls: list, hostnames: list, cur: DictCursor):
+    query = "INSERT INTO hosts (url, hostname) VALUES (%s, %s)"
+    data = list(zip(urls, hostnames))
+    execute_batch(cur, query, data)
+
+def create_table(cur: DictCursor):
+    query = '''
+    CREATE TABLE IF NOT EXISTS hosts(
+        url VARCHAR,
+        hostname VARCHAR
+    )
+    '''
+    cur.execute(query)
 
 main()
