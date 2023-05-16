237c16241c50c655bee860ba25e596e47edf32e2
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-05-16 11:09:46 +0200

    Added tests in ExtractorVCTest.java

--- analyzer/src/test/java/nl/tudelft/ExtractorVCTest.java
+++ analyzer/src/test/java/nl/tudelft/ExtractorVCTest.java
@@ -1,7 +1,10 @@
 package nl.tudelft;
 
-import nl.tudelft.mavensecrets.resolver.DefaultResolver;
+import nl.tudelft.mavensecrets.NopResolver;
+import org.apache.maven.model.DeploymentRepository;
+import org.apache.maven.model.DistributionManagement;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Scm;
 import org.junit.jupiter.api.*;
 
 import java.io.File;
@@ -13,6 +16,7 @@ import static org.mockito.Mockito.mock;
 class ExtractorVCTest {
     private static Extractor extractor;
     private static Maven maven;
+    private static Model testModel;
     private static File file;
     private static String pkgName = "";
     private static Database db = mock(Database.class);
@@ -20,16 +24,30 @@ class ExtractorVCTest {
     @BeforeAll
     static void setUp() {
         extractor = new ExtractorVC();
-        maven = new Maven(new DefaultResolver("/.m2/test"));
+        maven = new Maven(NopResolver.getInstance());
+
+        Scm scm = new Scm();
+        scm.setUrl("https://github.com/ashkboos/MavenSecrets");
+        testModel = new Model();
+        testModel.setScm(scm);
+        testModel.setUrl("https://se.ewi.tudelft.nl/");
+
+        DistributionManagement dist = new DistributionManagement();
+        DeploymentRepository repo = new DeploymentRepository();
+        repo.setUrl("https://github.com/example/example");
+        dist.setRepository(repo);
+        testModel.setDistributionManagement(dist);
     }
 
     @AfterAll
     static void tearDown() {
+        extractor = null;
+        maven = null;
     }
 
     @Test
     public void testNumFields () throws IOException, SQLException {
-        try (Package pkg = createPackage(new Model())) {
+        try (Package pkg = createPackage(testModel)) {
             Object[] results = extractor.extract(maven, pkg, pkgName, db);
             Assertions.assertNotNull(results);
             Assertions.assertEquals(extractor.fields().length, results.length);
@@ -37,7 +55,23 @@ class ExtractorVCTest {
     }
 
     @Test
-    void extract() {
+    void testExtractAll() throws SQLException, IOException {
+        Package pkg = createPackage(testModel);
+        Object[] results = extractor.extract(maven, pkg, pkgName, db);
+        Assertions.assertNotNull(results);
+        Assertions.assertArrayEquals(new Object[] {
+                "https://github.com/ashkboos/MavenSecrets",
+                "https://se.ewi.tudelft.nl/",
+                "https://github.com/example/example"
+        }, results);
+    }
+
+    @Test
+    void testAllNull() throws SQLException, IOException {
+        Model emptyModel = new Model();
+        Package pkg = createPackage(emptyModel);
+        Object[] results = extractor.extract(maven, pkg, pkgName, db);
+        Assertions.assertArrayEquals(new Object[] {null, null, null}, results);
     }
 
     private static Package createPackage(Model model) {
