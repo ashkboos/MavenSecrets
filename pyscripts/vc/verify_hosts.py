a599f60422a7d526004c9e10412ffd3ff89d5213
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-05-18 17:03:13 +0200

    tried to convert to more functional style (WIP)

--- pyscripts/vc/verify_hosts.py
+++ pyscripts/vc/verify_hosts.py
@@ -1,8 +1,6 @@
 import subprocess
-import git
+import re
 
-from psycopg2.extensions import connection
-from psycopg2.extras import DictCursor
 from database import *
 
 class VerifyHost:
@@ -11,35 +9,64 @@ class VerifyHost:
         self.db = db
         self.TABLE = 'hosts'
         self.timeout = 30
+        self.funcs = [lambda x : x, lambda x: self.git_to_https(x), lambda x: self.http_to_https(x)]
 
-    
+    # TODO add to errortable
+    # TODO mark them as processed
     def verify_hosts(self):
         records = self.db.get_all()
         valid = 0
 
         for record in records:
+            success = False
             print('-'*50)
             url = record['url']
             print(url)
 
-            try:
-                process = subprocess.run(['env', 'GIT_TERMINAL_PROMPT=0', 'timeout', '30', 'git', 'ls-remote', url, 'HEAD'], 
-                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=self.timeout)
-            except TimeoutError:
-                print(f'TIMED OUT after {self.timeout}s!')
-
-            output = process.stdout.decode()
-            err = process.stderr.decode()
-            print(output)
-            if not err:
+            for func in self.funcs:
+                if self.tryWith(url, func):
+                    success = True
+            
+            if success:
                 valid += 1
-            else:
-                # Check if private or not
-                print(err)
 
-            print(f'Exit code: {process.returncode}')
         
         print(f'There were {valid} repos out of {len(records)}')
+
+
+    def run_cmd(self, url: str):
+        return subprocess.run(['env', 'GIT_TERMINAL_PROMPT=0', 'git', 'ls-remote', url, 'HEAD'], 
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=self.timeout)
+    
+
+    def tryWith(self, url: str, convert):
+        try:
+           url = convert(url)
+           process = self.run_cmd(url)
+        except subprocess.TimeoutExpired:
+            print(f'TIMED OUT after {self.timeout}s!')
+
+        output = process.stdout.decode()
+        err = process.stderr.decode()
+        print(output)
+        print(f'Exit code: {process.returncode}')
+        if not err:
+            return True
+        else:
+            print(err)
+            return False
+
+
+    def git_to_https(self, url: str) -> str:
+        n_url = re.sub(r"git://", "https://", url)
+        n_url = re.sub(r"git@", "https://", n_url)
+        return n_url
+
+
+    def http_to_https(self, url: str) -> str:
+        pass
+
+
 
 
 # Exceptions:
@@ -60,7 +87,7 @@ class VerifyHost:
 
 # git://github.com/instaclustr/instaclustr-icarus.git TIMES OUT
 # BUT
-# https://github.com/instaclustr/instaclustr-icarus.git DOESNT WORK
+# https://github.com/instaclustr/instaclustr-icarus.git WORKS
 
 
 
