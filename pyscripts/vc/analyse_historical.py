f33c635fdb3189ff41789de374bb904a19b3fb3b
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-05-25 12:52:27 +0200

    Merged hosts with tiny market shares into 'others' category

--- pyscripts/vc/analyse_historical.py
+++ pyscripts/vc/analyse_historical.py
@@ -12,29 +12,36 @@ class HistoricalAnalyzer:
 
 
     def analyse(self):
-        # records = self.db.collate_hosts_yearly()
-        records = self.gen_test_data()
         cmap = 'viridis'
+        threshold = 0.05
 
+        records = self.db.collate_hosts_yearly()
+        # records = self.gen_test_data()
         df = pd.DataFrame(records, columns=['host','count', 'year'])
-        df['market share'] = df.groupby('year')['count'].transform(lambda x: x / x.sum())
+        df['market_share'] = df.groupby('year')['count'].transform(lambda x: x / x.sum())
 
-        pivot_df = df.pivot(index='year', columns='host', values='market share')
+        below = df[df['market_share'] < threshold]
+        others = below.groupby('year').agg({'host': lambda x: ' '.join(['others']), 'count': sum, 'market_share': sum}).reset_index()
+        df = pd.concat([df, others])
+        df = df[~df.isin(below)].dropna()
+
+        pivot_df = df.pivot(index='year', columns='host', values='market_share')
         pivot_df.plot(kind='area', stacked=True, colormap=cmap)
-        print(df)
-        print(pivot_df)
         plt.show()
-        
+
     
     def gen_test_data(self):
         hosts = ['github.com', 'gitlab.com', 'gitee.com', 'gitlab.alibaba-inc.com', 
                  'bitbucket.org', 'gitbucket.com', 'git.com', 'git.us', 'gitman.sm']
 
         data = []
-        for year in range(2011,2023):
-            print(year)
+        for year in range(2011,2024):
             for host in list(set(hosts)):
                 count =  int(1000 * np.random.random_sample())
+                if np.random.binomial(1,0.25) == 1:
+                    count = None
+                if host == 'github.com':
+                    count = 5000
                 entry = [host, count,year]
                 data.append(entry)
         return data 
