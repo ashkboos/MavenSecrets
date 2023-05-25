60ba43b70b7da12de5ac0879df32975b1a1275c1
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-05-25 13:05:45 +0200

    hosts only grouped into 'others' category if more than 1 below threshold

--- pyscripts/vc/analyse_historical.py
+++ pyscripts/vc/analyse_historical.py
@@ -21,9 +21,10 @@ class HistoricalAnalyzer:
         df['market_share'] = df.groupby('year')['count'].transform(lambda x: x / x.sum())
 
         below = df[df['market_share'] < threshold]
-        others = below.groupby('year').agg({'host': lambda x: ' '.join(['others']), 'count': sum, 'market_share': sum}).reset_index()
-        df = pd.concat([df, others])
-        df = df[~df.isin(below)].dropna()
+        if len(below) > 1:
+            others = below.groupby('year').agg({'host': lambda x: ' '.join(['others']), 'count': sum, 'market_share': sum}).reset_index()
+            df = pd.concat([df, others])
+            df = df[~df.isin(below)].dropna()
 
         pivot_df = df.pivot(index='year', columns='host', values='market_share')
         pivot_df.plot(kind='area', stacked=True, colormap=cmap)
@@ -44,4 +45,29 @@ class HistoricalAnalyzer:
                     count = 5000
                 entry = [host, count,year]
                 data.append(entry)
+
+        # data = [
+        # ['host1', 11, 2021],
+        # ['host2', 26, 2021],
+        # ['host3', 38, 2021],
+        # ['host1', 11, 2022],
+        # ['host2', 24, 2022],
+        # ['host3', 32, 2022],
+        # ['host1', 18, 2023],
+        # ['host2', 20, 2023],
+        # ['host3', 32, 2023],
+        # ['host1', 14, 2024],
+        # ['host2', 26, 2024],
+        # ['host3', 37, 2024],
+        # ['host1', 39, 2025],
+        # ['host2', 10, 2025],
+        # ['host3', 35, 2025],
+        # ['host1', 18, 2026],
+        # ['host2', 29, 2026],
+        # ['host3', 33, 2026],
+        # ['host1', 14, 2027],
+        # ['host2', 26, 2027],
+        # ['host3', 1, 2027],
+        # ['host4', 1, 2027],
+        # ]
         return data 
