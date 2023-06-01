6e9808b04bd787f29f47227552a35fee0c6831af
ntomassen <n.h.c.tomassen@student.tudelft.nl>
2023-06-01 16:29:32 +0200

    Optimise some imports and stick to shrinkwrap

--- analyzer/src/main/java/nl/tudelft/mavensecrets/extractors/DependencyExtractor.java
+++ analyzer/src/main/java/nl/tudelft/mavensecrets/extractors/DependencyExtractor.java
@@ -1,21 +1,18 @@
 package nl.tudelft.mavensecrets.extractors;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 import nl.tudelft.mavensecrets.resolver.DefaultResolver;
-import nl.tudelft.mavensecrets.resolver.Resolver;
-import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
-import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.collection.CollectRequest;
+import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactResult;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 import org.eclipse.aether.resolution.DependencyResult;
@@ -33,6 +30,7 @@ public class DependencyExtractor implements Extractor {
     //private static final Logger LOGGER = LogManager.getLogger(DependencyExtractor.class);
     private DefaultResolver resolver = new DefaultResolver();
     private static final RemoteRepository MAVEN_CENTRAL = new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/").build();
+    private static List<RemoteRepository> repositories = List.of(MAVEN_CENTRAL);
     private final Field[] fields = {
             new Field("directdependencies", "INTEGER"),
             new Field("transitivedependencies", "INTEGER")
@@ -49,16 +47,11 @@ public class DependencyExtractor implements Extractor {
         Objects.requireNonNull(pkg);
         Object[] result = new Object[2];
         Model m = pkg.pom();
-        List<Dependency> dependencies = m.getDependencies();
+        List<org.apache.maven.model.Dependency> dependencies = m.getDependencies();
         int directDependencies = dependencies.size();
         String id = pkg.id().group() + ":" + pkg.id().artifact() + ":" + pkgType + ":" + pkg.id().version();
         List<MavenCoordinate> files = resolve(id);
-//        int trans = 0;
-//        try {
-//            trans = resolves(pkg.id().group(), pkg.id().artifact(), pkg.id().version());
-//        } catch (DependencyResolutionException e){
-//            System.out.println("error: " + e);
-//        }
+//        int trans = resolves(pkg.id().group(), pkg.id().artifact(), pkg.id().version());
         int a = files.size() - 1;
         if(a < 0) a = 0;
         result[0] = directDependencies;
@@ -81,14 +74,24 @@ public class DependencyExtractor implements Extractor {
         return result;
     }
 
-    private int resolves(String groupId, String artifactId, String version) throws DependencyResolutionException {
-        CollectRequest r = new CollectRequest();
-        Artifact a = resolver.createArtifact(groupId, artifactId, version);
-        r.setRootArtifact(a);
-        DependencyRequest d = new DependencyRequest();
-        d.setCollectRequest(r);
-        DependencyResult result = resolver.getRepository().resolveDependencies(resolver.getRepositorySystemSession(), d);
-        return result.getArtifactResults().size();
-
+    private int resolves(String groupId, String artifactId, String version) {
+        Artifact artifact = resolver.createArtifact(groupId, artifactId, version);
+        RepositorySystem repository = resolver.getRepository();
+        RepositorySystemSession session = resolver.getRepositorySystemSession();
+        DependencyResult result;
+        List<Dependency> list = new ArrayList<>();
+        list.add(new Dependency(artifact, null));
+        try {
+            result = repository.resolveDependencies(session, new DependencyRequest(new CollectRequest((Dependency) null, list, repositories), null));
+        } catch (DependencyResolutionException exception) {
+            // Handle exception
+            return -1;
+        }
+        List<Artifact> transDeps = new ArrayList<>();
+        for (ArtifactResult ar : result.getArtifactResults()) {
+            Artifact a = ar.getArtifact();
+            transDeps.add(a);
+        }
+        return transDeps.size();
     }
 }
