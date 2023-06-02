a2de830769a1116c8778376c2199840219ea7a8c
Priyam Rungta <p.rungta@student.tudelft.nl>
2023-06-03 01:38:37 +0200

    Script for different packaging type in index and repo

--- pyscripts/demography_packaging.py
+++ pyscripts/demography_packaging.py
@@ -23,20 +23,114 @@ def main():
 
 
 def packaging_analysis(cur):
-    # Frequency of pairs of difference in packaging type from pom and index
-    print_result_with_individual_freq(cur)
-
     # Individual frequency of each packaging type from pom
     print_plot_packaging_pom(cur)
 
-    # Individual frequency of each packaging type from index
-    print_frequency_from_index(cur)
+    # Individual frequency of each packaging type from repo
+    print_frequency_from_repo(cur)
 
     # Frequency of packages with frequency of packaging types
-    print_frequency_of_packages_with_frequency_packaging_type(cur)
+    print_frequency_of_packages_with_frequency_packaging_types(cur)
+
+    # Frequency of pairs of difference in packaging type from pom and index
+    print_difference_with_individual_freq(cur)
+
+    # Number of different packages where index != repo
+    print_frequency_index_packaging_different_repo(cur)
 
 
-def print_result_with_individual_freq(cur):
+def print_plot_packaging_pom(cur):
+    cur.execute('SELECT packagingtypefrompom, COUNT(*) FROM packages WHERE packagingtypefrompom IS NOT NULL GROUP BY '
+                'packagingtypefrompom')
+    results = cur.fetchall()
+    # Create a dictionary to store the "packagingType" values and their counts
+    packaging_type_counts = {}
+    distinct_packaging_types = set()
+
+    for row in results:
+        packaging_type = row[0]
+        count = row[1]
+        packaging_type_counts[packaging_type] = count
+        distinct_packaging_types.add(packaging_type)
+
+    unique_packaging_type_count = len(distinct_packaging_types)
+
+    print('PACKAGING TYPE FROM POM')
+    print()
+
+    for packaging_type, count in packaging_type_counts.items():
+        print(f'Packaging type: {packaging_type}, Count: {count}')
+    print()
+
+    print(f'Total number of unique packaging types in pom: {unique_packaging_type_count}')
+    print('---x----')
+    print()
+
+    # Create a bar chart
+    packaging_type_counts = {key: value for key, value in packaging_type_counts.items() if value is not None}
+    plt.bar(packaging_type_counts.keys(), packaging_type_counts.values())
+
+    # Add labels and title
+    plt.xlabel('Packaging Type')
+    plt.ylabel('Count')
+    plt.title('Number of Packages by Packaging Type')
+    plt.xticks(rotation=45, ha='right')
+    x_tick_labels = [packaging_type[:5] + '...' if len(packaging_type) > 5 else packaging_type for packaging_type in
+                     packaging_type_counts.keys()]
+    plt.xticks(list(packaging_type_counts.keys()), x_tick_labels)
+
+    # Show the chart
+    plt.show()
+
+
+def print_frequency_from_repo(cur):
+    cur.execute("SELECT allpackagingtype FROM packages")
+
+    all_packagingtype_list = [row['allpackagingtype'] for row in cur.fetchall()]
+
+    sorted_frequencies = frequency_of_each_word(all_packagingtype_list)
+
+    unique_words_count = len(sorted_frequencies)
+
+    print('PACKAGING TYPE FROM THE REPO')
+    print()
+
+    # Print the word frequencies
+    for word, frequency in sorted_frequencies:
+        print(f'Packaging type: {word} - Frequency: {frequency}')
+    print()
+
+    print(f'Number of unique packaging types from the repo: {unique_words_count}')
+    print('---x----')
+    print()
+
+
+def print_frequency_of_packages_with_frequency_packaging_types(cur):
+    # Execute a query to fetch all values from the 'allpackagingtype' column in the table
+    cur.execute("SELECT allpackagingtype FROM packages")
+
+    # Create a dictionary to store the frequency of packaging types for each row
+    row_frequency = collections.defaultdict(int)
+
+    # Iterate over the rows and count the distinct packaging types for each row
+    for row in cur.fetchall():
+        allpackagingtype = row['allpackagingtype']
+        if allpackagingtype is not None:
+            packaging_types = set(allpackagingtype.strip('[]').split(','))
+            num_packaging_types = len(packaging_types)
+            row_frequency[num_packaging_types] += 1
+
+    print('FREQUENCY OF PACKAGES WITH DIFFERENT PACKAGING TYPES')
+    print()
+
+    # Print the frequency of rows with each distinct count of packaging types
+    for num_packaging_types, frequency in row_frequency.items():
+        print(f'Frequency of packages with {num_packaging_types} packaging types: {frequency}')
+    print('---x----')
+    print()
+
+
+def print_difference_with_individual_freq(cur):
     cur.execute('SELECT packagingtypefrompom, packagingtypefromrepo, COUNT(*) FROM packages WHERE '
                 'packagingtypefrompom != packagingtypefromrepo GROUP BY '
                 'packagingtypefrompom, packagingtypefromrepo')
@@ -61,92 +155,30 @@ def print_result_with_individual_freq(cur):
     print()
 
 
-def print_plot_packaging_pom(cur):
-    cur.execute('SELECT packagingtypefrompom, COUNT(*) FROM packages WHERE packagingtypefrompom IS NOT NULL GROUP BY '
-                'packagingtypefrompom')
-    results = cur.fetchall()
-    # Create a dictionary to store the "packagingType" values and their counts
-    packaging_type_counts = {}
+def print_frequency_index_packaging_different_repo(cur):
+    # Execute a query to fetch the values from the 'allpackagingtype' and 'packagingtypefromrepo' columns
+    cur.execute("SELECT allpackagingtype, packagingtypefromrepo FROM packages")
 
-    for row in results:
-        packaging_type = row[0]
-        count = row[1]
-        packaging_type_counts[packaging_type] = count
+    count = 0
 
-    total_count = sum(packaging_type_counts.values())
-
-    print('PACKAGING TYPE FROM POM')
-    print()
-
-    for packaging_type, count in packaging_type_counts.items():
-        print(f'Packaging type: {packaging_type}, Count: {count}')
-    print()
-
-    print(f'Total number of packages analysed: {total_count}')
-    print('---x----')
-    print()
-
-    # Create a bar chart
-    packaging_type_counts = {key: value for key, value in packaging_type_counts.items() if value is not None}
-    plt.bar(packaging_type_counts.keys(), packaging_type_counts.values())
-
-    # Add labels and title
-    plt.xlabel('Packaging Type')
-    plt.ylabel('Count')
-    plt.title('Number of Packages by Packaging Type')
-    plt.xticks(rotation=45, ha='right')
-    x_tick_labels = [packaging_type[:5] + '...' if len(packaging_type) > 5 else packaging_type for packaging_type in
-                     packaging_type_counts.keys()]
-    plt.xticks(list(packaging_type_counts.keys()), x_tick_labels)
-
-    # Show the chart
-    plt.show()
-
-
-def print_frequency_from_index(cur):
-    cur.execute("SELECT allpackagingtype FROM packages")
-
-    all_packagingtype_list = [row['allpackagingtype'] for row in cur.fetchall()]
-
-    sorted_frequencies = frequency_of_each_word(all_packagingtype_list)
-
-    unique_words_count = len(sorted_frequencies)
-
-    print('PACKAGING TYPE FROM THE REPO')
-    print()
-
-    # Print the word frequencies
-    for word, frequency in sorted_frequencies:
-        print(f'Packaging type: {word} - Frequency: {frequency}')
-    print()
-
-    print(f'Number of unique packaging types from the repo: {unique_words_count}')
-    print('---x----')
-    print()
-
-
-def print_frequency_of_packages_with_frequency_packaging_type(cur):
-    # Execute a query to fetch all values from the 'allpackagingtype' column in the table
-    cur.execute("SELECT allpackagingtype FROM packages")
-
-    # Create a dictionary to store the frequency of packaging types for each row
-    row_frequency = collections.defaultdict(int)
-
-    # Iterate over the rows and count the distinct packaging types for each row
+# Iterate over the rows and check if the values are the same
     for row in cur.fetchall():
         allpackagingtype = row['allpackagingtype']
+        packagingtypefromindex = row['packagingtypefromrepo']
+
         if allpackagingtype is not None:
-            packaging_types = set(allpackagingtype.strip('[]').split(','))
-            num_packaging_types = len(packaging_types)
-            row_frequency[num_packaging_types] += 1
+            # Remove the square brackets and split the 'allpackagingtype' string into a list
+            allpackagingtype_list = allpackagingtype.strip('[]').split(',')
 
-    print('FREQUENCY OF PACKAGES WITH DIFFERENT PACKAGING TYPES')
-    print()
+            # Remove any leading or trailing whitespaces from each value in the 'allpackagingtype_list'
+            allpackagingtype_list = [val.strip() for val in allpackagingtype_list]
 
-    # Print the frequency of rows with each distinct count of packaging types
-    for num_packaging_types, frequency in row_frequency.items():
-        print(f'Frequency of packages with {num_packaging_types} packaging types: {frequency}')
-    print('---x----')
+            # Check if 'packagingtypefromindex' does not match all the values in 'allpackagingtype_list'
+            if set(allpackagingtype_list) != {packagingtypefromindex}:
+                count += 1
+
+    print(f'Number of packages where the packaging type in the index is not same as packaging type(s) on repo: {count}')
+    print('----x----')
     print()
 
 
