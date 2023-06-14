173ad1f7362aa76a562ed0d3f724428d09ed4808
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2023-06-14 11:27:53 +0200

    fixed log spam

--- pyscripts/vc/verify_hosts.py
+++ pyscripts/vc/verify_hosts.py
@@ -31,8 +31,8 @@ class VerifyHost:
             self.log.info("Tasks submitted...")
             for future in as_completed(futures):
                 checkpoint += 1
-                if checkpoint % 10 == 0:
-                    self.log.critical(f"Checkpoint: Processed {checkpoint} packages...")
+                if checkpoint % 1000 == 0:
+                    self.log.info(f"Checkpoint: Processed {checkpoint} packages...")
                 success = future.result()
 
             self.log.info("All done. Thread Pool shutting down...")
